8c462d9794e3afce080ac3260c7ebab03e696f7e
pacarti <59978338+pacarti@users.noreply.github.com>
2021-05-31 21:21:47 +0200

    Added finding second equally closest value
    If there is any other as closest value to average as first, then the program lists it also now

--- closestToAvg.py
+++ closestToAvg.py
@@ -9,6 +9,13 @@ def compareToAvg(array, average):
         arrayOfDifferences.append(abs(array[i] - average))
     return arrayOfDifferences
 
+def checkAnotherValues(comparedValues, minimalComparedValue, fiveCounts, theClosestValue):
+    comparedValues.remove(minimalComparedValue)
+    fiveCounts.remove(theClosestValue)
+    for i in range(len(comparedValues)):
+        if comparedValues[i] == minimalComparedValue:
+           return fiveCounts[i]
+
 
 fiveCounts = []
 for i in range (5):
@@ -18,13 +25,16 @@ for i in range (5):
 average = avg(fiveCounts)
 comparedValues = compareToAvg(fiveCounts, average)
 minimalComparedValue = min(comparedValues)
+# print(minimalComparedValue)
 indexOfClosestValue = comparedValues.index(minimalComparedValue)
 theClosestValue = fiveCounts[indexOfClosestValue]
 if theClosestValue.is_integer() == True:
     theClosestValue = int(theClosestValue)    
-
+anotherMinValue = checkAnotherValues(comparedValues, minimalComparedValue, fiveCounts, theClosestValue)
 
 print("Average:", average)
-print("Differences list: ", comparedValues)
-# print("The minimal value of differences between average and value is: ", minimalComparedValue)
-print("The closest value:", theClosestValue)
+#print("Differences list: ", comparedValues)
+#print("The minimal value of differences between average and value is: ", minimalComparedValue)
+print("The closest value:", theClosestValue, end = '')
+if anotherMinValue != None and anotherMinValue != theClosestValue:
+    print(" and", anotherMinValue)
